4aed8617ed060751fd35c5e3c0ac70c16bf1b8f6
prontLin <timtimtim127543@gmail.com>
2026-05-24 21:09:38 +0800

    chore(lint): add ESLint and Prettier configuration for Astro

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,29 +2,31 @@ import js from "@eslint/js"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 import { defineConfig } from "eslint/config"
+import eslintPluginAstro from "eslint-plugin-astro"
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-        plugins: { js },
-        extends: ["js/recommended"],
-        languageOptions: { globals: globals.browser },
-        rules: {
-            semi: ["error", "never"],
-            quotes: ["error", "double"],
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    args: "all",
-                    argsIgnorePattern: "^_",
-                    caughtErrors: "all",
-                    caughtErrorsIgnorePattern: "^_",
-                    destructuredArrayIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                    ignoreRestSiblings: true,
-                },
-            ],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      semi: ["error", "never"],
+      quotes: ["error", "double"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
         },
+      ],
     },
-    tseslint.configs.recommended,
+  },
+  tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
 ])
